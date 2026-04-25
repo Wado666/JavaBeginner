5f769d146979acfb866de4dc00eb79d41d908863
Wado666 <minhtungn12@gmail.com>
2026-04-25 23:16:13 +0700

    Add files via upload

---
+++ XeChoHang.java
@@ -0,0 +1,54 @@
+// Nguyen Minh Tung - 24820100059
+package QuanLyPhuongTien;
+import Libary.InputException;
+import java.util.Scanner;
+
+public class XeChoHang extends XeDichVu{
+    private double trongTai;
+    private double soKmVanChuyen;
+
+    public XeChoHang() {
+        super();
+    }
+    public XeChoHang(String bienSoXe, double donGiaCoBan, int soChoNgoi, double trongTai, double soKmVanChuyen ) {
+        super(bienSoXe, donGiaCoBan,  soChoNgoi);
+        this.trongTai = trongTai;
+        this.soKmVanChuyen = soKmVanChuyen;
+    }
+
+    public XeChoHang(XeChoHang obj) {
+        super(obj);
+        this.trongTai = obj.trongTai;
+        this.soKmVanChuyen = obj.soKmVanChuyen;
+    }
+
+    public double getTrongTai() {
+        return trongTai;
+    }
+
+    public void setTrongTai(double trongTai) {
+        this.trongTai = trongTai;
+    }
+
+    public double getSoKmVanChuyen() {
+        return soKmVanChuyen;
+    }
+
+    public void setSoKmVanChuyen(double soKmVanChuyen) {
+        this.soKmVanChuyen = soKmVanChuyen;
+    }
+    public double tinhDoanhThu(){
+        return donGiaCoBan + (soKmVanChuyen*trongTai*5000);
+    }
+    public void input(){
+    super.input();
+    trongTai = InputException.inputDoublePositive("Nhap so tai trong: ");
+    soKmVanChuyen = InputException.inputDoublePositive("Nhap KM: ");
+}
+    public void output(){
+        System.out.print(this);
+    }
+    public String toString() {
+        return "Xe cho hang: " + super.toString() + ", Tai trong: " + trongTai + ", So KM: " + soKmVanChuyen + ", Doanh thu: " + tinhDoanhThu();
+    }
+}
